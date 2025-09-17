ddc385f3abf045558a07f952ffbb6a0a72a0c070
TucoT9 <186250778+T9Tuco@users.noreply.github.com>
2025-09-18 01:08:43 +0200

    Add files via upload

---
+++ dwm/Themes/gray.h
@@ -0,0 +1,5 @@
+static const char col_gray1[] = "#12151a"; // Background (Bar, Inactive Window)
+static const char col_gray2[] = "#e6f1ff"; // Border inactive Window
+static const char col_gray3[] = "#7a7a7a"; // Textcolor (normal Window, Bar)
+static const char col_gray4[] = "#e6f1ff"; // Textcolor active window (focused)
+static const char col_mainColor[]	= "#12151a"; // Main Color
